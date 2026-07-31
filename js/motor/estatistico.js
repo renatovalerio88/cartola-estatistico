44e6a97bbe46e41c7d706a3c544401f2de873357
renatovalerio88 <valerio.renato@hotmail.com>
2026-07-31 17:49:06 -0300

    Update estatistico.js

--- js/motor/estatistico.js
+++ js/motor/estatistico.js
@@ -1,1 +1,13 @@
+<script src="js/utils.js"></script>
+<script src="js/ui.js"></script>
 
+<script src="js/motor/estatistico.js"></script>
+
+<script src="js/recomendacoes/dados.js"></script>
+<script src="js/recomendacoes/ranking.js"></script>
+<script src="js/recomendacoes/cards.js"></script>
+
+<script src="js/escalacoes/dados.js"></script>
+<script src="js/escalacoes/cards.js"></script>
+
+<script src="js/app.js"></script>
