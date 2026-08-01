64e62ee9493b01d2e8c9bb416b1ed80de85a709c
renatovalerio88 <valerio.renato@hotmail.com>
2026-08-01 12:14:15 -0300

    Create piso_teto.js

---
+++ js/motor/piso_teto.js
@@ -0,0 +1,50 @@
+/*
+======================================================
+CARTOLA ESTATÍSTICO
+Motor de Piso e Teto
+======================================================
+*/
+
+const MotorPisoTeto = (() => {
+
+    function calcular(historico){
+
+        if(!historico?.length){
+
+            return {
+
+                piso:0,
+                teto:0
+
+            };
+
+        }
+
+        const pontos =
+            historico.map(
+                j=>j.pontos||0
+            );
+
+        return{
+
+            piso:
+                MotorMetricas.minimo(
+                    pontos
+                ),
+
+            teto:
+                MotorMetricas.maximo(
+                    pontos
+                )
+
+        };
+
+    }
+
+    return{
+
+        calcular
+
+    };
+
+})();
