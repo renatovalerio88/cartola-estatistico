8042d86271eebbb379b43e0511c33849609cefc4
renatovalerio88 <valerio.renato@hotmail.com>
2026-08-01 10:51:23 -0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -57,7 +57,7 @@ async function inicializarHistorico() {
             HistoricoCards.renderResumo(
                 HistoricoMetricas.calcular(metricas)
             );
-
+            HistoricoCards.renderTabela(numero);
         }
 
     }
