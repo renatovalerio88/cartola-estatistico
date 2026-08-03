5961010b91a288e00a9b4150f8277de6faabc449
renatovalerio88 <valerio.renato@hotmail.com>
2026-08-03 16:35:24 -0300

    Update escalacao.js

--- js/motor/escalacao.js
+++ js/motor/escalacao.js
@@ -10,53 +10,134 @@ const MotorEscalacao = (() => {
     const FORMACOES = {
 
         "4-4-2": {
-            GOL: 1,
-            LAT: 2,
-            ZAG: 2,
-            MEI: 4,
-            ATA: 2,
-            TEC: 1
+            GOL:1,
+            LAT:2,
+            ZAG:2,
+            MEI:4,
+            ATA:2,
+            TEC:1
         },
 
         "3-4-3": {
-            GOL: 1,
-            LAT: 0,
-            ZAG: 3,
-            MEI: 4,
-            ATA: 3,
-            TEC: 1
+            GOL:1,
+            LAT:0,
+            ZAG:3,
+            MEI:4,
+            ATA:3,
+            TEC:1
         }
 
     };
 
-    function montar(jogadores, formacao) {
+    function score(j){
+
+        return Number(
+
+            j.projecao ??
+
+            j.score ??
+
+            0
+
+        );
+
+    }
+
+    function montar(
+
+        jogadores,
+
+        formacao,
+
+        patrimonio = Infinity
+
+    ){
 
         const esquema =
+
             FORMACOES[formacao];
 
-        if (!esquema) {
+        if(!esquema){
+
             return [];
+
         }
 
         const titulares = [];
 
-        for (const posicao of Object.keys(esquema)) {
+        let custo = 0;
+
+        for(const posicao of Object.keys(esquema)){
 
             const quantidade =
+
                 esquema[posicao];
 
-            jogadores
-                .filter(j => j.posicao === posicao)
-                .sort(
-                    (a, b) =>
-                        (b.score || 0) -
-                        (a.score || 0)
-                )
-                .slice(0, quantidade)
-                .forEach(
-                    jogador =>
-                        titulares.push(jogador)
+            const candidatos =
+
+                jogadores
+
+                    .filter(
+
+                        j=>j.posicao===posicao
+
+                    )
+
+                    .sort(
+
+                        (a,b)=>
+
+                            score(b)-score(a)
+
+                    );
+
+            for(const jogador of candidatos){
+
+                if(
+
+                    titulares.filter(
+
+                        t=>t.posicao===posicao
+
+                    ).length
+
+                    >= quantidade
+
+                ){
+
+                    break;
+
+                }
+
+                const preco =
+
+                    Number(
+
+                        jogador.preco || 0
+
+                    );
+
+                if(
+
+                    custo + preco >
+
+                    patrimonio
+
+                ){
+
+                    continue;
+
+                }
+
+                titulares.push(
+
+                    jogador
+
                 );
+
+                custo += preco;
+
+            }
 
         }
 
@@ -64,7 +145,7 @@ const MotorEscalacao = (() => {
 
     }
 
-    return {
+    return{
 
         montar
 
